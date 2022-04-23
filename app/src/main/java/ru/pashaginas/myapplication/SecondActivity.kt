3cb2e99ad55f38ab42af9cc04174ba85dce1a66d
grusha4669 <grusha4669@gmail.com>
2022-04-23 13:21:27 +0300

    add button and fields ,hind text, localized

--- app/src/main/java/ru/pashaginas/myapplication/SecondActivity.kt
+++ app/src/main/java/ru/pashaginas/myapplication/SecondActivity.kt
@@ -4,13 +4,18 @@ import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AbsListView
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         var costId = intent.getIntExtra("COST_ID", 0)
-        Log.e("TAG", "Cost Id = " + costId)
+        Log.e("TAG", "Cost Id = $costId")
+
     }
 }
