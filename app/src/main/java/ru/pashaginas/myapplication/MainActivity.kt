3d652c2fa9d6e14d4fce600acc591284fffa28d4
grusha4669 <grusha4669@gmail.com>
2022-04-24 11:46:47 +0300

    added into music

--- app/src/main/java/ru/pashaginas/myapplication/MainActivity.kt
+++ app/src/main/java/ru/pashaginas/myapplication/MainActivity.kt
@@ -4,21 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.app.ShareCompat
-import java.nio.file.attribute.AclFileAttributeView
+import android.media.MediaPlayer
+import android.widget.Button
+import android.widget.Toast
+
+var mPlayer: MediaPlayer? = null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mPlayer = MediaPlayer.create(this, R.raw.imperial_march)
+        mPlayer!!.start()
     }
     fun tapScreen(view: View) {
-        val intent = Intent(this,SecondActivity::class.java)
+        val intent = Intent(this,SecondActivity::class.java,)
         intent.putExtra("COST_ID", 1)
         startActivity(intent)
-
-//        setContentView(R.layout.activity_second)
-        // shows second screen if main screen was tapped
     }
 }
-// TODO Recycleview
