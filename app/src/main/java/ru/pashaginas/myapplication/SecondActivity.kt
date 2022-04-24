3ffc1b52ee36d6ac4701b12c6c64a68c9c284925
grusha4669 <grusha4669@gmail.com>
2022-04-24 21:13:46 +0300

    add selector and toast logig if button is pressed

--- app/src/main/java/ru/pashaginas/myapplication/SecondActivity.kt
+++ app/src/main/java/ru/pashaginas/myapplication/SecondActivity.kt
@@ -1,5 +1,6 @@
 package ru.pashaginas.myapplication
 
+import android.graphics.drawable.Drawable
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,14 +8,26 @@ import android.util.Log
 import android.view.View
 import android.widget.AbsListView
 import android.widget.Button
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        button = findViewById(R.id.button)
+        button.setOnClickListener(listener)
+
         var costId = intent.getIntExtra("COST_ID", 0)
         Log.e("TAG", "Cost Id = $costId") //what?
+    }
+        val listener = View.OnClickListener { view ->
+        when (view.getId()) {
+            R.id.button -> {
+                Toast.makeText(applicationContext, "Button pressed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
