69ef0c8cad923bd808ae906abcd5cc1386a82cbd
grusha4669 <grusha4669@gmail.com>
2022-04-26 22:03:14 +0300

    changed design added selector and toast msg

--- app/src/main/java/ru/pashaginas/myapplication/MainActivity.kt
+++ app/src/main/java/ru/pashaginas/myapplication/MainActivity.kt
@@ -4,26 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.media.MediaPlayer
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
-var mPlayer: MediaPlayer? = null
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mPlayer = MediaPlayer.create(this, R.raw.imperial_march)
-        mPlayer!!.isLooping = true
-        mPlayer!!.start()
+
     }
+
     fun tapScreen(view: View) {
-        val intent = Intent(this,SecondActivity::class.java,)
+        val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra("COST_ID", 1)
         startActivity(intent)
-        mPlayer!!.stop()
-        mPlayer = MediaPlayer.create(this, R.raw.nodarthvader)
-        mPlayer!!.start()
     }
 }
