b2e63821b6cd0d86807fcc433b86c32390db5934
grusha4669 <grusha4669@gmail.com>
2022-05-09 15:54:55 +0300

    tabLayout with swipes realized using viewBinding

--- app/src/main/java/ru/pashaginas/myapplication/MainActivity.kt
+++ app/src/main/java/ru/pashaginas/myapplication/MainActivity.kt
@@ -1,21 +1,26 @@
 package ru.pashaginas.myapplication
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var itemsAdapter: MoneyItemsAdapter
     private lateinit var fab: FloatingActionButton
+    private lateinit var adapter: ViewPagerAdapter
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+
 
     companion object {
         const val RCODE = 500
@@ -48,8 +53,22 @@ class MainActivity : AppCompatActivity() {
         recyclerView.addItemDecoration(
             DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
         )
-//        val vp: ViewPager2 = findViewById(R.id.viewpager)
-//        vp.adapter = ViewPagerAdapter()
+        adapter = ViewPagerAdapter(this)
+        viewPager = findViewById(R.id.pager)
+        viewPager.adapter = adapter
+
+        tabLayout = findViewById(R.id.tab_layout)
+
+        TabLayoutMediator(tabLayout,viewPager) {tab, position ->
+            tab.text = when(position) {
+                0 -> getString(R.string.fragment_a)
+                1 -> getString(R.string.fragment_b)
+                else -> {
+                    throw Resources.NotFoundException("Fragment not found")
+                }
+            }
+
+        }.attach()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/ru/pashaginas/myapplication/ViewPagerAdapter.kt
+++ app/src/main/java/ru/pashaginas/myapplication/ViewPagerAdapter.kt
@@ -1,18 +1,28 @@
 package ru.pashaginas.myapplication
 
+import android.content.res.Resources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter : FragmentStateAdapter(FragmentActivity()) {
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
-        if (position == 0) {
-            return FragmentA()
-        } else if (position == 1) {
-            return FragmentA()
+//        if (position == 0) {
+//            return FragmentA()
+//        } else if (position == 1) {
+//            return FragmentB()
+//        }
+//        return FragmentA()
+
+        return when (position) {
+            0 -> FragmentA()
+            1 -> FragmentB()
+            else -> {
+                throw Resources.NotFoundException("Fragment not found")
+            }
         }
-        return FragmentA()
     }
 }
